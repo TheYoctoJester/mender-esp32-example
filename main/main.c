3aee64d96deaefac1bef7126611471b362be832b
Josef Holzmayr <jester@theyoctojester.info>
2023-01-30 16:55:29 +0100

    tinkering: everything works

--- main/main.c
+++ main/main.c
@@ -35,20 +35,78 @@
 #include "protocol_examples_common.h"
 #include "led_strip.h"
 #include "sdkconfig.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "driver/uart.h"
 
 #include "mender-glue.h"
 #include "stats.h"
-#include "ssd1306.h"
+#include "ssd1306.h"S
 
 /**
  * @brief Tag used for logging
  */
 static const char *TAG = "main";
 
-#define I2C_MASTER_SCL_IO 26        /*!< gpio number for I2C master clock */
-#define I2C_MASTER_SDA_IO 25        /*!< gpio number for I2C master data  */
+#define I2C_MASTER_SCL_IO 33        /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 34        /*!< gpio number for I2C master data  */
 #define I2C_MASTER_NUM I2C_NUM_1    /*!< I2C port number for master dev */
 #define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
+
+/**
+ * Brief:
+ * This test code shows how to configure gpio and how to use gpio interrupt.
+ *
+ * GPIO status:
+ * GPIO18: output
+ * GPIO19: output
+ * GPIO4:  input, pulled up, interrupt from rising edge and falling edge
+ * GPIO5:  input, pulled up, interrupt from rising edge.
+ *
+ * Test:
+ * Connect GPIO18 with GPIO4
+ * Connect GPIO19 with GPIO5
+ * Generate pulses on GPIO18/19, that triggers interrupt on GPIO4/5
+ *
+ */
+#define GPIO_INPUT_IO_0     19
+#define GPIO_INPUT_IO_1     20
+#define GPIO_INPUT_IO_2     21
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1) | (1ULL<<GPIO_INPUT_IO_2))
+#define ESP_INTR_FLAG_DEFAULT 0
+
+#define GPIO_OUTPUT_IO_0    18
+#define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_0)
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%d] intr, val 19/20/21: %d/%d/%dS\n",
+                io_num,
+                gpio_get_level(GPIO_INPUT_IO_0),
+                gpio_get_level(GPIO_INPUT_IO_1),
+                gpio_get_level(GPIO_INPUT_IO_2)
+            );
+        }
+    }
+}
+
+//ADC Channels
+#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_0
+static const char *TAG_CH[2][10] = {{"ADC1_CH6"}, {"ADC2_CH0"}};
+
+//ADC Attenuation
+#define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
 
 static ssd1306_handle_t ssd1306_dev = NULL;
 
@@ -71,9 +129,100 @@ display() {
     ssd1306_clear_screen(ssd1306_dev, 0x00);
 
     char data_str[10] = {0};
-    sprintf(data_str, "C STR");
-    ssd1306_draw_string(ssd1306_dev, 70, 16, (const uint8_t *)data_str, 16, 1);
+    sprintf(data_str, "Mender");
+    ssd1306_draw_string(ssd1306_dev, 32, 16, (const uint8_t *)data_str, 16, 1);
     ssd1306_refresh_gram(ssd1306_dev);
+}
+
+#define CONFIG_BLINK_LED_RMT_CHANNEL 0
+#define BLINK_GPIO 17
+
+static uint8_t s_led_state = 0;
+static led_strip_t *pStrip_a;
+
+static void blink_led(void)
+{
+    s_led_state = (s_led_state + 1) % 2;
+    /* If the addressable LED is enabled */
+    if (s_led_state) {
+        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+        pStrip_a->set_pixel(pStrip_a, 0, 128, 0, 0);
+        pStrip_a->set_pixel(pStrip_a, 1, 0, 128, 0);
+        pStrip_a->set_pixel(pStrip_a, 2, 0, 0, 128);
+        /* Refresh the strip to send data */
+        pStrip_a->refresh(pStrip_a, 100);
+    } else {
+        /* Set all LED off to clear all pixels */
+        pStrip_a->clear(pStrip_a, 50);
+    }
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
+    /* LED strip initialization with the GPIO and pixels number*/
+    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 3);
+    /* Set all LED off to clear all pixels */
+    pStrip_a->clear(pStrip_a, 50);
+}
+/**
+ * This is an example which echos any data it receives on configured UART back to the sender,
+ * with hardware flow control turned off. It does not use UART driver event queue.
+ *
+ * - Port: configured UART
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: off
+ * - Pin assignment: see defines below (See Kconfig)
+ */
+
+#define ECHO_TEST_TXD 44
+#define ECHO_TEST_RXD 43
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+
+#define ECHO_UART_PORT_NUM      0
+#define ECHO_UART_BAUD_RATE    115200
+#define ECHO_TASK_STACK_SIZE    2048
+
+#define BUF_SIZE (1024)
+
+static void echo_task(void *arg)
+{
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = ECHO_UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    int intr_alloc_flags = 0;
+
+#if CONFIG_UART_ISR_IN_IRAM
+    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
+
+    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_RATE_MS);
+        // Write data back to the UART
+        uart_write_bytes(ECHO_UART_PORT_NUM, (const char *) data, len);
+        if (len) {
+            data[len] = '\0';
+            ESP_LOGI(TAG, "Recv str: %s", (char *) data);
+        }
+    }
 }
 
 /**
@@ -95,6 +244,56 @@ app_main(void) {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
+    //zero-initialize the config structure.
+    gpio_config_t o_conf = {};
+    //disable interrupt
+    o_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    o_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    o_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    o_conf.pull_down_en = 0;
+    //disable pull-up mode
+    o_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&o_conf);
+    
+    //zero-initialize the config structure.
+    gpio_config_t i_conf = {};
+    // pin 19, rot A - both edges
+    //interrupt of rising edge
+    i_conf.intr_type = GPIO_INTR_ANYEDGE;
+    //bit mask of the pins, use GPIO19/20/21 here
+    i_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    i_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    i_conf.pull_up_en = 1;
+    //configure GPIO with the given settings
+    gpio_config(&i_conf);
+
+    //change gpio intrrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_2, GPIO_INTR_NEGEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_2, gpio_isr_handler, (void*) GPIO_INPUT_IO_2);
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+
+    //ADC1 config
+    ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
+
     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
      * examples/protocols/README.md for more information about this function.
@@ -104,12 +303,33 @@ app_main(void) {
 	/* Start the Mender client */
 	init_mender_client();
 
+    display();
+
+    /* Configure the peripheral according to the LED type */
+    configure_led();
+
+    xTaskCreate(echo_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
+
     /* Infinite loop, print stats periodically */
+    ESP_LOGI(TAG, "Entering main loop.");
+    int cnt = 0;
     while (1) {
+
+        static int last = 0;
+        int current = adc1_get_raw(ADC1_EXAMPLE_CHAN0);
+        if (abs(last - current) > 100) {
+            ESP_LOGI(TAG_CH[0][0], "raw adc read: %d", current);
+        }
+        last = current;
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        //gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
+        //gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
+        
+        blink_led();
+        cnt++;
+
         /* Print stats */
         print_stats();
-
-        /* Wait before next snapshot */
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
